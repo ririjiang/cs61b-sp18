3797324ffe818e082f1801cb069af3d4a3b5499c
Richie <475478129@qq.com>
2022-06-29 14:55:02 +0800

    completed proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,36 +6,36 @@ public class ArrayDeque<T> {
     public ArrayDeque() {
         this.item = (T[]) new Object[8];
         size = 0;
-        nextFirst =3;
-        nextLast = 4;
-    }
-    public void firstlesslast(int i){
-        T[] Narray = (T[]) new Object[i];
-        System.arraycopy(this.item,0, Narray, 0, this.size);
-        nextFirst++;
-        this.item = Narray;
+        nextFirst =0;
+        nextLast = 1;
     }
 
-    public void firstlargelast(int i){
-        T[] Narray = (T[]) new Object[i];
-        System.arraycopy(this.item,0,Narray, 0, this.nextLast-1);
-        System.arraycopy(this.item,Narray.length-this.size+nextFirst,Narray, 0, size-1-nextFirst);
-        nextFirst = Narray.length-this.size+nextFirst;
-        this.item = Narray;
+    public int minusone(int index){
+        if(index == 0){
+           return index = this.item.length-1;
+        }
+        else {return index = index-1;}
     }
+
+    public int addone(int index){
+        if(index == this.item.length-1){
+            return index = 0;
+        }
+         return index = index+1;
+        }
     //resize array
-    public void resize(){
-            if(nextFirst< nextLast|nextLast==0){
-                firstlesslast(this.size*4);
-            }
-            else{
-               firstlargelast(this.size*4);
-            }
+    public void resize(int changelength){
+        T[] Narray = (T[]) new Object[changelength];
+        System.arraycopy(this.item,0,Narray, 0,this.minusone(nextLast)+1);
+        System.arraycopy(this.item,this.addone(nextFirst),Narray, this.addone(Narray.length-this.size+nextFirst), this.item.length-1-nextFirst);
+        nextFirst = (Narray.length-1)-(item.length-nextFirst-1);
+        nextLast = this.minusone(nextLast)+1;
+        this.item = Narray;
     }
 
     public void addFirst(T thing){
         if(size == item.length){
-            resize();
+            resize(this.size*4);
             item [nextFirst] =thing;
             nextFirst--;
             size++;
@@ -57,7 +57,7 @@ public class ArrayDeque<T> {
     /** Inserts X into the back of the list. */
     public void addLast(T thing) {
         if(size == item.length){
-            resize();
+            resize(this.size*4);
             item [nextLast] =thing;
             nextLast++;
             size++;
@@ -76,31 +76,63 @@ public class ArrayDeque<T> {
         }
     }
 
-    /** Returns the item from the back of the list. */
-    public int getLast() {
-        return 0;
+    public boolean isEmpty(){
+        return size==0;
     }
+
     /** Gets the ith item in the list (0 is the front). */
-    public T get(T i) {
-        return i;
+    public T get(int i) {
+        return item[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
         return size;
     }
+    //print the list from the first to the last
+    public void printDeque(){
+        int x = this.addone(nextFirst);
+        while(x == minusone(nextLast)){
+            System.out.println(item[x]);
+            x = this.addone(x);
+        }
+    }
+
+    /** Deletes item from front of the list and
+     * returns deleted item. */
+    public T removeFirst(){
+        int i = this.addone(nextFirst);
+        T removedFront = item[i];
+        item[nextFirst+1] = null;
+        size--;
+        nextFirst = this.addone(nextFirst);
+        double ratio = (double)size/this.item.length;
+        if(ratio <= 0.25 ){
+            resize(this.item.length/4);
+        }
+        return removedFront;
+    }
 
     /** Deletes item from back of the list and
      * returns deleted item. */
-    public int removeLast() {
-        return 0;
+    public T removeLast(){
+        T removedBack = item[this.minusone(nextLast)];
+        item[this.minusone(nextLast)] = null;
+        size--;
+        nextLast = this.minusone(nextLast);
+        if((double)size/this.item.length <= 0.25 ){
+            resize(this.item.length/4);
+        }
+        return removedBack;
     }
+
 
     public static void main(String[] args) {
         ArrayDeque<Integer> a1 = new ArrayDeque<>();
         a1.addLast(3);
         a1.addLast(3);
-        a1.addLast(3); a1.addLast(3);
+        a1.addLast(3);
+        a1.addLast(3);
         a1.addLast(3);
         a1.addFirst(3);
         a1.addLast(3);
@@ -108,6 +140,10 @@ public class ArrayDeque<T> {
         a1.addFirst(3);
         a1.addLast(3);
         a1.addLast(3);
+        a1.removeFirst();
+        a1.removeLast();
+        a1.removeLast();
+        a1.removeLast();
 
     }
 }
